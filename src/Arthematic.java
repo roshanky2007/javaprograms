6256d0f097aee18d87bb0507dad1429498686e8c
Roshanky2007 <roshanky2007@gmail.com>
2024-12-21 21:01:20 +0530

    Adding

---
+++ src/Arthematic.java
@@ -0,0 +1,13 @@
+public class Arthematic {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("There are three arthematic operators:");
+        System.out.println("The addition of a and b = "+(a+b));
+        System.out.println("The subtraction of a and b = "+(a-b));
+        System.out.println("The  of a and b = "+(a*b));
+        System.out.println("The addition of a and b = "+(a/b));
+        System.out.println("The addition of a and b = "+(a%b));
+
+    }
+}
+    
