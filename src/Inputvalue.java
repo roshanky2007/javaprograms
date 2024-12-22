892a59934a4d7bf93de8fc04686aead9d9b92262
Roshanky2007 <roshanky2007@gmail.com>
2024-12-22 18:18:00 +0530

    Rename

--- src/Inputvalue.java
+++ src/Inputvalue.java
@@ -1,6 +1,6 @@
     import java.util.Scanner;
 
-    public class Main {
+    public class Inputvalue {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter your name: ");
