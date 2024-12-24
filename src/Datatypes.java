443095ca32bad604fc5c48067540f58ffff51487
Roshanky2007 <roshanky2007@gmail.com>
2024-12-24 19:02:18 +0530

    Data types

---
+++ src/Datatypes.java
@@ -0,0 +1,10 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=0; i<n; i++)
+        {
+            System.out.println("5");
+        }
+    }
+    
+}
---
+++ src/Loop/Forloop.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class Forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("5");
+        }
+    }
+
+    
+}
