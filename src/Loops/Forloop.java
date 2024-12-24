fffa2a070327b68a33e3744f16e5bda75570fa2f
Roshanky2007 <roshanky2007@gmail.com>
2024-12-24 21:29:26 +0530

    Move to Operators

--- src/Loops/Forloop.java
+++ src/Loops/Forloop.java
@@ -1,4 +1,4 @@
-package Loop;
+package Loops;
 
 public class Forloop {
     public static void main(String[] args) {
--- src/Operators/Arthematic.java
+++ src/Operators/Arthematic.java
@@ -1,7 +1,8 @@
+package Operators;
 public class Arthematic {
     public static void main(String[] args) {
         int a = 10, b = 20;
-        System.out.println("There are three arthematic operators:");
+        System.out.println("There are five arthematic operators:");
         System.out.println("The addition of a and b = "+ (a+b));
         System.out.println("The subtraction of a and b = " + (a-b));
         System.out.println("The multiplication of a and b = " + (a*b));
